090ffb82223174880a7bfc15b589c07995d5be21
Harper <HarperMelissaM@gmail.com>
2025-06-09 16:29:08 -0500

    assignment completed

--- src/main/java/org/example/BetterStudentManager.java
+++ src/main/java/org/example/BetterStudentManager.java
@@ -6,16 +6,19 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BetterStudentManager {
     public List<Student> students = new ArrayList<>();
 
     public void addStudent(String name, double grade) {
         // This method should add a new student to the list of students
+        students.add(new Student(name, grade));
     }
 
     public void removeStudent(String name) {
         // This method should remove the student with the given name from the list of students
+        students.removeIf(student -> Objects.equals(student.name, name));
     }
 
     public String getStudentList() {
@@ -29,6 +32,8 @@ public class BetterStudentManager {
         // looping through the list of students
         //
         // Replace the following line with your implementation
-        return null;
+        StringBuilder studentList = new StringBuilder ();
+        for (Student student : students) studentList.append(student.name + " " + student.grade + "\n");
+        return studentList.toString();
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Student {
+    // instance variables
+    String name;
+    double grade;
+
+    // constructors
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+
+}
